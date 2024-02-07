0c686f4d82cb76dc696c48f20e29deb35a3d4a87
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2024-02-07 10:16:27 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -3,50 +3,71 @@
 # 
 # Masivi https://mape.gov.lv/catalog/materials/6501426F-B6EC-44B3-8B93-DC553DAE8886/view?preview=7A90D16F-0A8A-4840-A2E3-5EA4F6D4E194
 # Lists https://www.w3schools.com/python/python_lists.asp
-# 
-
-def add(list, item):
-  # https://www.w3schools.com/python/python_lists_add.asp
-  pass
 
 
-def remove(list, index):
-  # https://www.w3schools.com/python/python_lists_remove.asp
-  pass
+def add(list):
+  additem = input("What do you want to add to list?(one item)")
+  list.append(additem)
+pass
+
+
+def remove(list):
+  int(list = [ ])
+  index = int(input("What you want to remove?(write the number of item)\n"))
+  index = index + 1
+  int(index)
+  list.pop(index)
+pass
 
 
 def clear(list):
-  # https://www.w3schools.com/python/python_lists_remove.asp
-  pass
+  list.clear()
+pass
 
 
 def print_list(list):
-  # https://www.w3schools.com/python/python_lists_loop.asp
-  pass
+  print(list)
+pass
 
 
 def show(list):
-  # https://www.w3schools.com/python/python_lists_access.asp
-  pass
+  int(list = [ ])
+  print(list)
+pass
 
-list = []
+
+
+
+
+list = [ ]
 print("List is empty now, what you want to do?")
 while True:
   choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n5. Show item by index"))
   if choice == 1:
-    item = input("What you want to add?\n")
-    add(list, item)
-    print_list(list)
+    add(list)
+    print(list)
+  # https://www.w3schools.com/python/python_lists_add.asp
+
+
   elif choice == 2:
-    index = int(input("What you want to remove?\n"))
-    remove(list, index)
+    remove(list)
     print_list(list)
+  # https://www.w3schools.com/python/python_lists_remove.asp
+
+
   elif choice == 3:
     clear(list)
     print_list(list)
+  # https://www.w3schools.com/python/python_lists_remove.asp
+    
+
   elif choice == 4:
     print_list(list)
+  # https://www.w3schools.com/python/python_lists_loop.asp
+
   elif choice == 5:
     show(list)
+
+
   else:
     print("Invalid input")
